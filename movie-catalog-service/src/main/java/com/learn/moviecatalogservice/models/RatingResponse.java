44346b5a98b7a523c2783d15fd7751504c91ba63
Ashutosh Anand <2018ugee022@nitjsr.ac.in>
2022-10-01 10:29:22 +0530

    added dummy data for both the services in movie catalog service

---
+++ movie-catalog-service/src/main/java/com/learn/moviecatalogservice/models/RatingResponse.java
@@ -0,0 +1,27 @@
+package com.learn.moviecatalogservice.models;
+
+public class RatingResponse {
+    private String movieId;
+    private Integer rating;
+
+    public RatingResponse(String movieId, Integer rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
--- movie-catalog-service/src/main/java/com/learn/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/learn/moviecatalogservice/resources/CatalogResource.java
@@ -1,13 +1,16 @@
 package com.learn.moviecatalogservice.resources;
 
 import com.learn.moviecatalogservice.models.CatalogItem;
+import com.learn.moviecatalogservice.models.RatingResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // to use this class to handle routes
 @RequestMapping("/catalog")
@@ -18,14 +21,19 @@ public class CatalogResource {
 
         // get all rated movies from ratings data service -> it will have movie ids
         // creating a dummy list for now
+        List<RatingResponse> ratings = Arrays.asList(
+                new RatingResponse("1234", 4),
+                new RatingResponse("4329", 5)
+        );
 
         // for each movie id call movie info service and get details
         // add movie info and rating to Catalog item and return response
-        // as a list of Catalog item
+        // as a list of Catalog item (adding dummy data for now)
+        List<CatalogItem> response =
+                ratings.stream().map(rating -> new CatalogItem("3 idiots", "engineering life", 5))
+                .collect(Collectors.toList());
 
-        return Collections.singletonList(
-                new CatalogItem("3 idiots", "engineering life", 5)
-        );
+        return response;
     }
 }
 
