5e6fa1cd5b9f7d0384b83b5570b757da82ec9665
Ashutosh Anand <2018ugee022@nitjsr.ac.in>
2022-10-01 10:05:08 +0530

    created dummy movie info api

---
+++ movie-info-service/src/main/java/com/learn/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,17 @@
+package com.learn.movieinfoservice.resources;
+
+import com.learn.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId, "3 idiots");
+    }
+}
